0191b1bc4d404a95cf8a0417843fbe4e631585d4
Uzair Aftab <uzaaft@outlook.com>
2025-03-29 20:05:48 +0100

    feat: Add in ssh keys

--- users/ivare/nixos.nix
+++ users/ivare/nixos.nix
@@ -22,7 +22,14 @@
   users.users.ivare = {
     isNormalUser = true;
     home = "/home/ivare";
-    extraGroups = ["wheel" "docker" "lxd"];
+    extraGroups = ["wheel" "docker"];
     shell = pkgs.zsh;
+    # TODO: I took these keys from your github. Remove the ones you dont need.
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMDNN1wpwnpRwoR8Uv6Nj3LA9AwK49VCwZaisbFjmVi0"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPJPHAayzDjKefWsQOEFYSOAkx7FtoNRJz6UAEuO8JWy"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBTpaGmDAzLeCdXhr3uDwGZezxpE78Lu4JewvHgyB12P"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHd2THj0rBxiXn9Y5qCSmtpCCR00LpBLUIQNWGRw8HDj"
+    ];
   };
 }
